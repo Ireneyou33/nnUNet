35115cffdb57f505f84a10f2c24f35d4b04e03d0
NingAnMe <ninganme@qq.com>
2021-08-11 13:27:02 +0000

    增加nnUNetTrainerV2_CoTr_ResTrans

--- nnunet/training/network_training/nnUNetTrainer.py
+++ nnunet/training/network_training/nnUNetTrainer.py
@@ -712,7 +712,7 @@ class nnUNetTrainer(NetworkTrainer):
         global_dc_per_class = [i for i in [2 * i / (2 * i + j + k) for i, j, k in
                                            zip(self.online_eval_tp, self.online_eval_fp, self.online_eval_fn)]
                                if not np.isnan(i)]
-        self.all_val_eval_metrics.append(np.mean(global_dc_per_class))
+        self.all_val_eval_metrics.append(np.mean(global_dc_per_class))  #TODO 这里可以调整每个类别的权重
 
         self.print_to_log_file("Average global foreground Dice:", [np.round(i, 4) for i in global_dc_per_class])
         self.print_to_log_file("(interpret this as an estimate for the Dice of the different classes. This is not "
